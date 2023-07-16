7464c6f1e637e510a680a91ffbb3e4dcd9f393ee
sramteare <theguy_sr@yahoo.com>
2023-07-16 18:06:16 -0400

    longest common prefix

--- src/dsa.rs
+++ src/dsa.rs
@@ -1,3 +1,4 @@
 pub mod two_sum;
 pub mod palindrome_number;
 pub mod roman_to_int;
+pub mod longest_common_prefix;
---
+++ src/dsa/longest_common_prefix.rs
@@ -0,0 +1,52 @@
+pub fn longest_common_prefix(input_strs: Vec<String>) -> String {
+    if input_strs.len() == 1 {
+        return input_strs[0].clone();
+    }
+    let mut min_len = usize::MAX;
+    let strs: Vec<_> = input_strs.iter().map(|s|{
+        min_len = std::cmp::min(min_len, s.len());
+        return s.chars().collect::<Vec<_>>();
+    }).collect();
+    println!("Converted to Vec<Vec<char>> {:?}", strs);
+
+    for i in 0..min_len{
+        let ch = strs[0][i];
+        
+        for j in 1..strs.len() {
+            let cur = strs[j][i];
+            println!("{} {} {} {}", ch, cur, i, j);
+            if ch != cur {
+                return String::from(&input_strs[0][..i]);
+            }
+        }
+    }
+    String::from(&input_strs[0][..min_len])
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_longest_common_prefix1(){
+        assert_eq!(super::longest_common_prefix(vec![
+            "flower".to_string(),"flow".to_string(),"flight".to_string()
+        ]), "fl");
+    }
+    #[test]
+    fn test_longest_common_prefix2(){
+        assert_eq!(super::longest_common_prefix(vec![
+            "ab".to_string(),"a".to_string()
+        ]), "a");
+    }
+    #[test]
+    fn test_longest_common_prefix_one_string(){
+        assert_eq!(super::longest_common_prefix(vec![
+            "flower".to_string()]), "flower");
+    }
+    #[test]
+    fn test_longest_common_prefix_none_common(){
+        assert_eq!(super::longest_common_prefix(vec![
+            "dog".to_string(),"racecar".to_string(),"car".to_string()
+        ]), "");
+        
+    }
+}
